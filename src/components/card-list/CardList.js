f435b9e6d8de40f8884d6a0cde9d54bc447ad484
Hasnat Hoque <hasnathoque9@gmail.com>
2020-03-24 20:33:51 +0000

    initial commit with github pages

---
+++ src/components/card-list/CardList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './CardList.css'
+
+// components
+import {Card} from '../card/Card'
+
+export const CardList = props => {
+  return (
+    <div className="card-list">
+      {
+        props.monsters.map(monster =>
+          <Card key={monster.id} monster={monster}/>
+        )
+      }
+    </div>
+  )
+};

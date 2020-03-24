39acbc742e9b66ab036a5428ca0b61fd5741b7ad
Hasnat Hoque <hasnathoque9@gmail.com>
2020-03-24 20:33:39 +0000

    Updates

---
+++ precache-manifest.65d75a89a3f179a07be15610fa6823e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac7680b93327d5a396899628caba691",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ec8070e1fedb3e746408",
+    "url": "/react-monsters-rolodex/static/css/main.f86d6113.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec8070e1fedb3e746408",
+    "url": "/react-monsters-rolodex/static/js/main.bd1a7fef.chunk.js"
+  },
+  {
+    "revision": "84d462a1ad20fdb403f4",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.6eda5f3f.js"
+  }
+]);
